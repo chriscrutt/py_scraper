662e34b6a7d3e32be00d7edfbb1a4cc736465727
Chris <chriscrutt@gmail.com>
2020-12-23 16:31:07 -0800

    remove n = 1 test

--- apy.py
+++ apy.py
@@ -20,7 +20,8 @@ def get_balances(client):
     return [btc_f, btc_l, wbtc_f, wbtc_l]
 
 
-start = 1608769023841
+start = 1608769615688
+
 
 def num_orders(client):
 
@@ -72,7 +73,6 @@ def init(apis):
         P = s_btc + (s_wbtc) / avg_price  # Principal (P)
         t = (round(time()) - start / 1000) / 31557600  # time passed in years
         n = orders_filled / t  # Compound (n): (orders filled/time passed)
-        n = 1
 
         apy = n * ((A / P)**(1 / (n * t)) - 1)
         b_apy = n * (((A - bnb) / P)**(1 / (n * t)) - 1)
