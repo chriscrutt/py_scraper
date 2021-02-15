47ff84766625099d2599fa7a22cefcd6c2285c87
Chris <chriscrutt@gmail.com>
2021-02-14 23:58:52 -0800

    000

--- Main_new_start.py
+++ Main_new_start.py
@@ -10,10 +10,10 @@ app = Flask(__name__)
 # decorating index function with the app.route
 @app.route('/')
 def index():
-    return eh(1613361662, "s_s")
+    return eh(1613361662000, "s_s")
 
 # og restart first trade 1611908665000
-# new first trade 1613361662
+# new first trade 1613361662000
 
 if __name__ == "__main__":
     app.run("104.9.116.163", 82)
